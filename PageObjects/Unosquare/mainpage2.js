50daf6ccf8a8f13b2efea8e8d27edaa8c62987f3
Daniel Perez <daniel.perez@unosquare.com>
2023-06-12 11:32:49 -0600

    Complete the course

--- PageObjects/Unosquare/mainpage2.js
+++ PageObjects/Unosquare/mainpage2.js
@@ -1,7 +1,7 @@
 var testUnosquare = {
     testInUnosquare: function(){
         this.api.pause(1000)
-        const form = getData(); // this is the important part
+        const form = getData();
         return this
             .click('@blogMenu')
             .assert.elementPresent('@headerBlog')
@@ -22,17 +22,36 @@ var testUnosquare = {
     }
 }
 
+var testAmazon = {
+    testInAmazone: function(){
+        this.api.pause(1000)
+        const form = getData();
+        return this
+        .setValue('@searchBar',form.product)
+        .click('@menuOptions')
+        .assert.visible('@resultsVisible')
+        .moveToElement('@firstPrice', 10, 10)
+        .getText('@firstPrice')
+        .click('@productTitle')
+        .assert.textContains('@secondPrice',form.price)
+        .click('@addToCartButton')
+        .click('@noThanksButton')
+        .click('@goToCartButton')
+        .assert.textContains('@thirdPrice',form.price)
+        .click('@removeButton')
+    }
+}
+
 function getData() {
-    return require('../../dataExternal/unosquareForm'); // Using the correct path is important
+    return require('../../dataExternal/unosquareForm');
 };
 
 module.exports = {
     url: 'https://www.unosquare.com',
-    commands: [testUnosquare],
+    commands: [testUnosquare,testAmazon],
     elements: {
         blogMenu: "li[id='menu-item-3416'] a[class='ekit-menu-nav-link']",
         headerBlog: "h1[class='elementor-heading-title elementor-size-default']",
-        linkAssert: "https://www.unosquare.com/blog/",
         firstArticle: "article[class='elementor-post elementor-grid-item post-7799 post type-post status-publish format-standard has-post-thumbnail hentry category-project-management category-quality-assurance'] h3[class='elementor-post__title'] a",
         secondArticle: "article[class='elementor-post elementor-grid-item post-7764 post type-post status-publish format-standard has-post-thumbnail hentry category-software-development category-testing tag-software-development tag-testing tag-uat'] h3[class='elementor-post__title'] a",
         contactMenu: "div[class='elementor-element elementor-element-fdff709 elementor-hidden-mobile elementor-widget__width-auto elementor-widget elementor-widget-button'] span[class='elementor-button-text']",
@@ -40,11 +59,21 @@ module.exports = {
         emailInput: "#email-b0570355-5abf-47d4-b00c-fcf04db071cc",
         messageInput: "#message-b0570355-5abf-47d4-b00c-fcf04db071cc",
         logoPage: "img[alt='unosquare logo']",
-        aboutUrl: "https://www.unosquare.com/about-us/",
         name1: "div[class='elementor-element elementor-element-dfc5cb5 elementor-position-left elementor-vertical-align-middle elementor-widget elementor-widget-image-box'] h3[class='elementor-image-box-title']",
         name2: "div[class='elementor-element elementor-element-4527ae6 elementor-position-left elementor-vertical-align-middle elementor-widget elementor-widget-image-box'] h3[class='elementor-image-box-title']",
         name3: "div[class='elementor-element elementor-element-264d1f8 elementor-position-left elementor-vertical-align-middle elementor-widget elementor-widget-image-box'] h3[class='elementor-image-box-title']",
         name4: "div[class='elementor-element elementor-element-f95b1d6 elementor-position-left elementor-vertical-align-middle elementor-widget elementor-widget-image-box'] h3[class='elementor-image-box-title']",
-        name5: "div[class='elementor-element elementor-element-616fc80 elementor-position-left elementor-vertical-align-middle elementor-widget elementor-widget-image-box'] h3[class='elementor-image-box-title']"
+        name5: "div[class='elementor-element elementor-element-616fc80 elementor-position-left elementor-vertical-align-middle elementor-widget elementor-widget-image-box'] h3[class='elementor-image-box-title']",
+        searchBar: '#twotabsearchtextbox',
+        menuOptions: "div[aria-label='samsung galaxy note 20']",
+        resultsVisible: "div[class='s-widget-container s-spacing-small s-widget-container-height-small celwidget slot=MAIN template=SEARCH_RESULTS widgetId=search-results_1'] span[class='a-size-base-plus a-color-base a-text-normal']",
+        firstPrice: "div[class='s-widget-container s-spacing-small s-widget-container-height-small celwidget slot=MAIN template=SEARCH_RESULTS widgetId=search-results_1'] span[class='a-price-whole']",
+        productTitle: "div[class='s-widget-container s-spacing-small s-widget-container-height-small celwidget slot=MAIN template=SEARCH_RESULTS widgetId=search-results_1'] span[class='a-size-base-plus a-color-base a-text-normal']",
+        secondPrice: "body > div:nth-child(5) > div:nth-child(78) > div:nth-child(8) > div:nth-child(8) > div:nth-child(4) > div:nth-child(11) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > span:nth-child(1) > span:nth-child(2)",
+        addToCartButton: "#add-to-cart-button",
+        noThanksButton: '#attachSiNoCoverage > span > input',
+        goToCartButton: ".a-button-text[href='/cart?ref_=sw_gtc']",
+        thirdPrice: ".a-size-medium.a-color-base.sc-price.sc-white-space-nowrap.sc-product-price.a-text-bold",
+        removeButton: "input[value='Eliminar']"
     }
 }
--- test/lastTest/lastTest.js
+++ test/lastTest/lastTest.js
@@ -3,6 +3,11 @@ module.exports = {
         const unosquare = browser.page.mainpage2();
         unosquare.navigate().testInUnosquare();
         browser.end();
+    },
+
+    'Amazon test' : function(browser){
+        const amazon = browser.page.mainpage2();
+        amazon.navigate('https://www.amazon.com.mx/').testInAmazone();
+        browser.end();
     }
-    
 }
